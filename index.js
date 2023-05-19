3b9c1c7ff51605ba3b65b3b2b2fef534a6eb17c4
parsajabbari <parsajabbari@gmail.com>
2023-05-18 23:49:06 -0700

    feat: add prettier with auto sorting for package.json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hola, mundo!",
+        message: `${process.version}`,
         input: event,
       },
       null,
