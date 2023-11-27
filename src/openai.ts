914abcbb6b71af23f0400d6fbe9d652952407d5c
parsajabbari <parsajabbari@gmail.com>
2023-11-26 18:05:15 -0800

    config: still figuring out env varialbes with serverless

--- src/openai.ts
+++ src/openai.ts
@@ -1,23 +1,24 @@
 import OpenAI from "openai";
 
 export const handler = async () => {
-  const openai = new OpenAI();
+  return process.env.OPENAI_API_KEY;
+  // const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
-      },
-      {
-        role: "user",
-        content:
-          "Compose a poem that explains the concept of recursion in programming.",
-      },
-    ],
-    model: "gpt-3.5-turbo",
-  });
+  // const completion = await openai.chat.completions.create({
+  //   messages: [
+  //     {
+  //       role: "system",
+  //       content:
+  //         "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+  //     },
+  //     {
+  //       role: "user",
+  //       content:
+  //         "Compose a poem that explains the concept of recursion in programming.",
+  //     },
+  //   ],
+  //   model: "gpt-3.5-turbo",
+  // });
 
-  return completion;
+  // return completion;
 };
