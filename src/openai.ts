9a7aed9262581687f5ef9b698e7dbbfdf9b243ad
parsajabbari <parsajabbari@gmail.com>
2023-11-26 18:12:42 -0800

    feat: hello world chat completion handler

--- src/openai.ts
+++ src/openai.ts
@@ -1,24 +1,23 @@
 import OpenAI from "openai";
 
 export const handler = async () => {
-  return process.env.OPENAI_API_KEY;
-  // const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+  const openai = new OpenAI();
 
-  // const completion = await openai.chat.completions.create({
-  //   messages: [
-  //     {
-  //       role: "system",
-  //       content:
-  //         "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
-  //     },
-  //     {
-  //       role: "user",
-  //       content:
-  //         "Compose a poem that explains the concept of recursion in programming.",
-  //     },
-  //   ],
-  //   model: "gpt-3.5-turbo",
-  // });
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+      },
+      {
+        role: "user",
+        content:
+          "Compose a poem that explains the concept of recursion in programming.",
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
 
-  // return completion;
+  return completion;
 };
